bae689f915521f8ea06dde01e08ddab88186e4e5
Rafael Medeiros <rafaelmedrib@gmail.com>
2024-02-13 17:19:03 -0300

    add some essential models and migrations

---
+++ app/models/vendor.ts
@@ -0,0 +1,24 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
+import Deal from './deal.js'
+import type { HasMany } from '@adonisjs/lucid/types/relations'
+
+export default class Vendor extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare name: string
+
+  @column()
+  declare email: string
+
+  @hasMany(() => Deal)
+  declare deals: HasMany<typeof Deal>
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
