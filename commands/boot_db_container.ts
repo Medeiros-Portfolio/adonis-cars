13f4c2b9922095968a9f3397dc5570506f55c4de
Rafael Medeiros <rafaelmedrib@gmail.com>
2024-02-14 11:18:15 -0300

    add animated log to commands

--- commands/boot_db_container.ts
+++ commands/boot_db_container.ts
@@ -5,17 +5,29 @@ export default class BootDbContainer extends BaseCommand {
   static commandName = 'boot:db-container'
   static description = 'Start the database container for testing purposes'
 
+  animatedLog = this.logger.await('Starting the database container', {
+    suffix: 'boot:db-container',
+  })
+
   async run() {
-    this.logger.info('Starting the database container')
-    execSync('docker-compose up -d db', { stdio: 'inherit' })
+    this.animatedLog.start()
+
+    execSync('docker-compose up -d db', { stdio: 'ignore' })
+
+    this.animatedLog.update('Just one more second...')
+
+    await new Promise((resolve) => setTimeout(resolve, 1000))
   }
 
   async completed() {
     if (this.error) {
+      this.animatedLog.stop()
       this.logger.error('Database container failed to start')
       return true
     }
 
+    this.animatedLog.update('Done.')
+    this.animatedLog.stop()
     this.logger.success('Database container started')
 
     return true
