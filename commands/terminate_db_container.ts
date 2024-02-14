90f34f7e35f11a0840547e7072c342d07d09eb1a
Rafael Medeiros <rafaelmedrib@gmail.com>
2024-02-14 11:18:15 -0300

    add animated log to commands

--- commands/terminate_db_container.ts
+++ commands/terminate_db_container.ts
@@ -5,17 +5,24 @@ export default class TerminateDbContainer extends BaseCommand {
   static commandName = 'terminate:db-container'
   static description = 'Terminate the database container'
 
+  animatedLog = this.logger.await('Terminating the database container', {
+    suffix: 'terminate:db-container',
+  })
+
   async run() {
-    this.logger.info('Terminating the database container')
-    execSync('docker-compose down', { stdio: 'inherit' })
+    this.animatedLog.start()
+    execSync('docker-compose down', { stdio: 'ignore' })
   }
 
   async completed() {
     if (this.error) {
+      this.animatedLog.stop()
       this.logger.error('Database container failed to terminate')
       return true
     }
 
+    this.animatedLog.update('Done.')
+    this.animatedLog.stop()
     this.logger.success('Database container terminated')
 
     return true
