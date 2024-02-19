33fd258db43ba2636756ab646f8f625e200a7986
Rafael Medeiros <rafaelmedrib@gmail.com>
2024-02-19 09:13:49 -0300

    fix vendors seeder

--- database/seeders/vendor_seeder.ts
+++ database/seeders/vendor_seeder.ts
@@ -4,7 +4,13 @@ import { vendors } from '../../tests/mock/vendor.example.js'
 
 export default class extends BaseSeeder {
   async run() {
-    const { first, second } = vendors
-    await Vendor.createMany([first, second])
+    const { first, second, third } = vendors
+    const MOCK_PASSWORD = 'password'
+
+    const validVendors = [first, second, third].map((vendor) => {
+      return { ...vendor, password: MOCK_PASSWORD }
+    })
+
+    await Vendor.createMany(validVendors)
   }
 }
