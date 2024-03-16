6182af4041de411113ad4451ff9472c83ef5ad46
Rafael Medeiros <rafaelmedrib@gmail.com>
2024-03-15 22:03:39 -0300

    clean unusefull files

--- app/policies/car_policy.ts
+++
@@ -1,9 +0,0 @@
-import Vendor from '#models/vendor'
-import { BasePolicy } from '@adonisjs/bouncer'
-import { AuthorizerResponse } from '@adonisjs/bouncer/types'
-
-export default class CarPolicy extends BasePolicy {
-  create(vendor: Vendor, isVendor: boolean): AuthorizerResponse {
-    return isVendor
-  }
-}
--- tests/mock/manager.example.ts
+++
@@ -1,5 +0,0 @@
-export const managers = {
-  first: {
-    email: 'george.doe@notarealemail.no',
-  },
-}
