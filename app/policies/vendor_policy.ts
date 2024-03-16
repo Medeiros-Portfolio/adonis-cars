6d114d882b1087694ee8f750f262fca058deed25
Rafael Medeiros <rafaelmedrib@gmail.com>
2024-03-15 22:03:39 -0300

    clean unusefull files

--- app/policies/vendor_policy.ts
+++
@@ -1,9 +0,0 @@
-import Manager from '#models/manager'
-import { BasePolicy } from '@adonisjs/bouncer'
-import { AuthorizerResponse } from '@adonisjs/bouncer/types'
-
-export default class VendorPolicy extends BasePolicy {
-  create(user: Manager): AuthorizerResponse {
-    return user instanceof Manager
-  }
-}
--- tests/functional/info/info.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test } from '@japa/runner'
-
-test.group('Routes info', () => {
-  test('GET /', async ({ assert, client }) => {
-    const response = await client.get('/_info').send()
-
-    assert.equal(response.status(), 200)
-  })
-})
